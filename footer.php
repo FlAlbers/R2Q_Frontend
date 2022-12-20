351ba00489f75daaeadaa75d9428eb7dd103c4b6
unknown <fl-albers@web.de>
2022-12-20 17:04:36 +0100

    Impressum example added + Datenschutzerklärung example added + Impressum and Datenschutz linked in footer

--- footer.php
+++ footer.php
@@ -2,7 +2,9 @@
 
 <footer class="footer">
 <div class="footer_content1">
-<p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> Platzhalter 
+<p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> Platzhalter | 
+<a class= "footer_link" target='_blank'  href='impressum.php'>IMPRESSUM</a> | 
+<a class= "footer_link" target='_blank'  href='datenschutz.php'>DATENSCHUTZ</a> 
 <!-- <?php echo " / PHP Version: " . phpversion();?> -->
 </p>
 <p></p>
